90ab0ff90c7fcd0547fd25c0de15f0d2253e1ea4
ntw94 <db01004@naver.com>
2023-08-26 00:27:09 +0900

    first commit

---
+++ src/main/java/com/example/koreait/KoreaItApplication.java
@@ -0,0 +1,13 @@
+package com.example.koreait;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoreaItApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/koreait/KoreaItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.koreait;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreaItApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
